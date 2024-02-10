15fc72528aecd3454bf1e921d2efa2a55fd1b6aa
shorty <ungegrieftfirma@gmail.com>
2024-02-10 21:10:02 +0100

    - added PaperCore as dependency (local) - updated java version (17 -> 21)

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,13 +10,20 @@ description = "Game Template"
 
 java {
   // Configure the java toolchain. This allows gradle to auto-provision JDK 17 on systems that only have JDK 8 installed for example.
-  toolchain.languageVersion.set(JavaLanguageVersion.of(17))
+  toolchain.languageVersion.set(JavaLanguageVersion.of(21))
+}
+
+repositories {
+  mavenLocal()
 }
 
 dependencies {
   paperweight.paperDevBundle("1.20.4-R0.1-SNAPSHOT")
   // paperweight.foliaDevBundle("1.20.4-R0.1-SNAPSHOT")
   // paperweight.devBundle("com.example.paperfork", "1.20.4-R0.1-SNAPSHOT")
+
+  compileOnly("net.uniquepixels:core:latest")
+  compileOnly("net.uniquepixels:core-api:latest")
 }
 
 tasks {
@@ -30,7 +37,7 @@ tasks {
 
     // Set the release flag. This configures what version bytecode the compiler will emit, as well as what JDK APIs are usable.
     // See https://openjdk.java.net/jeps/247 for more information.
-    options.release.set(17)
+    options.release.set(21)
   }
   javadoc {
     options.encoding = Charsets.UTF_8.name() // We want UTF-8 for everything
