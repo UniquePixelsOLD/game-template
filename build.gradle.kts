4b22644bb8fb001a980c532dec25dd12048ad2e2
shorty <ungegrieftfirma@gmail.com>
2024-02-27 15:03:30 +0100

    update uniquepixels repo

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,11 +10,17 @@ description = "Game Template"
 
 java {
   // Configure the java toolchain. This allows gradle to auto-provision JDK 17 on systems that only have JDK 8 installed for example.
-  toolchain.languageVersion.set(JavaLanguageVersion.of(21))
+  toolchain.languageVersion.set(JavaLanguageVersion.of(17))
 }
 
 repositories {
-  mavenLocal()
+  mavenCentral()
+  maven("https://repo.uniquepixels.net/repository/minecraft") {
+    credentials {
+      username = "projectwizard"
+      password = System.getenv("UP_NEXUS_PASSWORD")
+    }
+  }
 }
 
 dependencies {
@@ -37,7 +43,7 @@ tasks {
 
     // Set the release flag. This configures what version bytecode the compiler will emit, as well as what JDK APIs are usable.
     // See https://openjdk.java.net/jeps/247 for more information.
-    options.release.set(21)
+    options.release.set(17)
   }
   javadoc {
     options.encoding = Charsets.UTF_8.name() // We want UTF-8 for everything
